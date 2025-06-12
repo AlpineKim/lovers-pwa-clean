1602a8e4c0b50d184311f218df4f6559f8c40194
AlpineKim <mrskimberlysimpson@gmail.com>
2025-06-11 20:17:56 -0600

    Add files via upload

--- firebase-firestore.js
+++ firebase-firestore.js
@@ -1,13 +1,11 @@
 const items = ["kissing", "holding hands", "cuddling", "oral", "toys", "dirty talk"];
 
 function saveResponse(uid, item, response) {
-  // Optimistically update UI
-  const btns = document.querySelectorAll(`[data-item='\${item}'] button`);
+  const btns = document.querySelectorAll(`[data-item='${item}'] button`);
   btns.forEach(btn => btn.classList.remove("selected"));
-  const selectedBtn = document.querySelector(`[data-item='\${item}'] button[data-response='\${response}']`);
+  const selectedBtn = document.querySelector(`[data-item='${item}'] button[data-response='${response}']`);
   if (selectedBtn) selectedBtn.classList.add("selected");
 
-  // Save to Firestore
   db.collection("checklists")
     .add({ uid, item, response, timestamp: firebase.firestore.FieldValue.serverTimestamp() });
 }
@@ -25,7 +23,10 @@ function loadChecklist(uid) {
       snapshot.forEach(doc => {
         const data = doc.data();
         if (!responses[data.item]) {
-          responses[data.item] = { response: data.response, timestamp: data.timestamp?.toDate().toLocaleString() || "" };
+          responses[data.item] = {
+            response: data.response,
+            timestamp: data.timestamp?.toDate().toLocaleString() || ""
+          };
         }
       });
 
@@ -34,17 +35,20 @@ function loadChecklist(uid) {
         div.className = "item";
         div.setAttribute("data-item", item);
         const meta = responses[item] ? `<span class="meta">(Last: ${responses[item].timestamp})</span>` : "";
-        div.innerHTML = \`\${item}:\${meta}
-          <button data-response="yes" onclick="saveResponse('\${uid}', '\${item}', 'yes')">Yes</button>
-          <button data-response="future" onclick="saveResponse('\${uid}', '\${item}', 'future')">Future</button>
-          <button data-response="maybe" onclick="saveResponse('\${uid}', '\${item}', 'maybe')">Maybe</button>
-          <button data-response="no" onclick="saveResponse('\${uid}', '\${item}', 'no')">No</button>
-          <span id="response-\${item}"></span>\`;
+        div.innerHTML = `${item}:${meta}
+          <button data-response="yes" onclick="saveResponse('${uid}', '${item}', 'yes')">Yes</button>
+          <button data-response="maybe" onclick="saveResponse('${uid}', '${item}', 'maybe')">Maybe</button>
+          <button data-response="no" onclick="saveResponse('${uid}', '${item}', 'no')">No</button>
+          <span id="response-${item}"></span>`;
         container.appendChild(div);
         if (responses[item]) {
-          const btn = div.querySelector(\`button[data-response='\${responses[item].response}']\`);
+          const btn = div.querySelector(`button[data-response='${responses[item].response}']`);
           if (btn) btn.classList.add("selected");
         }
       });
     });
 }
+
+// Globalize
+window.loadChecklist = loadChecklist;
+window.saveResponse = saveResponse;
