d669c184487183a4159d5e74c3edab4b48bc280e
AlpineKim <mrskimberlysimpson@gmail.com>
2025-06-11 18:27:42 -0600

    Delete firebase.js
    Supplanted by firebase-firestore.js

--- firebase.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "lovers-pwa.firebaseapp.com",
-  projectId: "lovers-pwa",
-  storageBucket: "lovers-pwa.appspot.com",
-  messagingSenderId: "1234567890",
-  appId: "1:1234567890:web:abcdef123456"
-};
